b7d1c47390c6ed80c08f305e2ad225f3f8b10c07
Frieder Pankratz <frieder.pankratz@gmail.com>
2020-06-21 15:08:32 +0200

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,56 @@
+from conans import ConanFile, CMake, tools
+
+
+class FastDDSConan(ConanFile):
+    name = "Fast-DDS-GEN"
+    version = "1.0.4"
+    license = "Apache License 2.0"
+    author = "Frieder Pankratz"
+    url = "https://github.com/TUM-CONAN/conan-fast-dds-gen.git"
+    description = "Conan wrapper for Fast-DDS-GEN"    
+    settings = "os"
+    options = {"shared": [True, False], "Build_Java" : [True, False]}
+    default_options = {"shared": True, "Build_Java" : False } #, "RPCProto" : "rpcdds"}
+    generators = "cmake"
+
+    exports_sources = "CMakeLists.txt", "cmake/*"
+
+    def source(self):
+        git = tools.Git()        
+        git.clone("https://github.com/eProsima/Fast-DDS-Gen.git", "master")#%self.version)
+
+            
+
+    def _configure_cmake(self):        
+        cmake = CMake(self)
+        cmake.verbose = True
+
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            cmake.definitions[var_name] = var_value        
+
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
+
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        #cmake = self._configure_cmake()
+        #cmake.build()
+        self.run("./gradlew assemble")
+
+    def package(self):
+        #cmake = self._configure_cmake()
+        #cmake.install()
+        
+        self.copy(src="build/libs", pattern="*.jar", dst="share/fastddsgen/java",keep_path=False)
+        self.copy(src="", pattern="LICENSE", dst="share/",keep_path=False)
+        self.copy(src="scripts", pattern="*", dst="bin",keep_path=False)
+        pass
+
+    def package_info(self):        
+        #self.cpp_info.libs = tools.collect_libs(self)
+        pass
