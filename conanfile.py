b8cfca0e61f73913a15160c7a1a7405203932ff1
Ulrich Eck <ulrich.eck@tum.de>
2023-04-24 18:23:01 +0200

    new package for conan2: fast-dds-gen

--- conanfile.py
+++ conanfile.py
@@ -1,63 +1,69 @@
-from conans import ConanFile, CMake, tools
-
+import os
+from conan import ConanFile
+from conan.tools.scm import Git
+from conan.tools.files import update_conandata, copy, chdir, collect_libs
+from conan.tools.layout import basic_layout
+from conan.tools.env import Environment
 
 class FastDDSConan(ConanFile):
-    name = "Fast-DDS-GEN"
-    version = "2.3.0"
+    name = "fast-dds-gen"
+    version = "2.4.0"
+    package_type = "application"
     license = "Apache License 2.0"
     author = "Frieder Pankratz"
     url = "https://github.com/TUM-CONAN/conan-fast-dds-gen.git"
     description = "Conan wrapper for Fast-DDS-GEN"    
+
     settings = "os"
-    options = {"shared": [True, False], "Build_Java" : [True, False]}
-    default_options = {"shared": True, "Build_Java" : False } #, "RPCProto" : "rpcdds"}
-    generators = "cmake"
+    generators = "VirtualBuildEnv"
+    no_copy_source = True
 
-    exports_sources = "CMakeLists.txt", "cmake/*"
+    def build_requirements(self):
+        self.build_requires("zulu-openjdk/11.0.15")
 
-    build_requires = (
-        "zulu-openjdk/11.0.15",
-        )
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "v{}".format(self.version),
+            "url": "https://github.com/eProsima/Fast-DDS-Gen.git"
+            }}
+            )
 
     def source(self):
-        git = tools.Git()        
-        git.clone("https://github.com/eProsima/Fast-DDS-Gen.git", "master")#%self.version)
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder, args=["--recursive", ])
+        git.checkout(commit=sources["commit"])
 
-            
+    def generate(self):
+        env = Environment()
+        return env
 
-    def _configure_cmake(self):        
-        cmake = CMake(self)
-        cmake.verbose = True
-
-        def add_cmake_option(option, value):
-            var_name = "{}".format(option).upper()
-            value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
-            cmake.definitions[var_name] = var_value        
-
-        for option, value in self.options.items():
-            add_cmake_option(option, value)
-
-        cmake.configure()
-        return cmake
+    def layout(self):
+        basic_layout(self, src_folder="source_subfolder")
 
     def build(self):
-        #cmake = self._configure_cmake()
-        #cmake.build()
-        if tools.os_info.is_windows:
-            self.run("gradlew.bat assemble")
-        else:
-            self.run("./gradlew assemble")
+        with chdir(self, self.source_folder):
+            if self.settings.os == "Windows":
+                self.run("gradlew.bat assemble")
+            else:
+                self.run("./gradlew assemble")
+
+    def package_id(self):
+        self.info.clear()
 
     def package(self):
-        #cmake = self._configure_cmake()
-        #cmake.install()
-        
-        self.copy(src="build/libs", pattern="*.jar", dst="share/fastddsgen/java",keep_path=False)
-        self.copy(src="", pattern="LICENSE", dst="share/",keep_path=False)
-        self.copy(src="scripts", pattern="*", dst="bin",keep_path=False)
-        pass
+        copy(self, pattern="*.jar",
+             dst=os.path.join(self.package_folder, "share", "fastddsgen", "java"),
+             src=os.path.join(self.source_folder, "build", "libs"),
+             keep_path=False)
+        copy(self, pattern="*",
+             dst=os.path.join(self.package_folder, "bin"),
+             src=os.path.join(self.source_folder, "scripts"),
+             keep_path=False)
+        copy(self, pattern="LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
 
-    def package_info(self):        
-        #self.cpp_info.libs = tools.collect_libs(self)
-        pass
+    def package_info(self):
+        self.cpp_info.includedirs = []
+        self.cpp_info.libdirs = []
